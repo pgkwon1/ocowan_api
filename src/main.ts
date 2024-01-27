8f92ed3083981fece36d07743ef09d9e1eeaf2f9
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-27 19:19:55 +0900

    feat: ValidationPipe  추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
 import { AxiosErrorFilter, TypeErrorFilter } from './common/common.catch';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   dotenv.config();
@@ -10,6 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new AxiosErrorFilter());
   app.useGlobalFilters(new TypeErrorFilter());
   await app.listen(3001);
