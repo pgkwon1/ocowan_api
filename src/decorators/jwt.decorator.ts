c06fb4e0dea39cd8cfd727ca8509c9d012dcccdd
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-29 21:48:26 +0900

    feat: jwt decorator 추가

---
+++ src/decorators/jwt.decorator.ts
@@ -0,0 +1,29 @@
+import {
+  BadRequestException,
+  ExecutionContext,
+  createParamDecorator,
+} from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+
+export const Jwt = createParamDecorator(
+  async (data, context: ExecutionContext) => {
+    const { headers } = context.switchToHttp().getRequest();
+    const tokens = headers.authorization.split(' ')[1];
+    const decodeTokens = await decodeJwt(tokens);
+    if (typeof decodeTokens === 'object') {
+      return decodeTokens.login;
+    }
+
+    throw new BadRequestException('invalid token');
+  },
+);
+
+function decodeJwt(token: string): Promise<string | jwt.JwtPayload> {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (error, decoded) => {
+      if (error) reject(error);
+
+      resolve(decoded);
+    });
+  });
+}
