c532fd31e19c3572a721a3904744634f39867c0d
pgkwon1@naver.com <pgkwon1@naver.com>
2024-02-15 21:36:48 +0900

    fix: 안쓰는 부분 삭제.

--- src/common/base.service.ts
+++ src/common/base.service.ts
@@ -1,11 +1,9 @@
-import { InjectModel } from '@nestjs/sequelize';
-import { CreateOptions, WhereOptions } from 'sequelize';
-import { Model, ModelCtor } from 'sequelize-typescript';
-import { MakeNullishOptional } from 'sequelize/types/utils';
+import { WhereOptions } from 'sequelize';
+import { Model } from 'sequelize-typescript';
 
 export default interface BaseService<T extends Model> {
   create(data: Partial<T>): Promise<T>;
-  update(data: Partial<T>, where: WhereOptions): Promise<T>;
+  update?(data: Partial<T>, where: WhereOptions): Promise<T>;
   delete(id: string, where: WhereOptions): Promise<number>;
   getAll(data: Partial<T>): Promise<T[]>;
   getOne(data: Partial<T>): Promise<T>;
