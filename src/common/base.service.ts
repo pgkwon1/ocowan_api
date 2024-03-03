d66592b0d4dea87cec0d7ea38d668d49fc6d6894
pgkwon1@naver.com <pgkwon1@naver.com>
2024-03-03 20:30:25 +0900

    update: base service getAll 파라미터 수정

--- src/common/base.service.ts
+++ src/common/base.service.ts
@@ -1,10 +1,10 @@
-import { WhereOptions } from 'sequelize';
+import { FindOptions, WhereOptions } from 'sequelize';
 import { Model } from 'sequelize-typescript';
 
 export default interface BaseService<T extends Model> {
   create(data: Partial<T>): Promise<T>;
   update?(data: Partial<T>, where: WhereOptions): Promise<T>;
   delete(id: string, where: WhereOptions): Promise<number>;
-  getAll(data: Partial<T>): Promise<T[]>;
+  getAll(options: FindOptions): Promise<T[]>;
   getOne(data: Partial<T>): Promise<T>;
 }
