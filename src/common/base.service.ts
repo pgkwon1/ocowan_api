52df0e67ad3d457ddb14a2de15c989c9c391668c
pgkwon1@naver.com <pgkwon1@naver.com>
2024-03-26 21:22:25 +0900

    update: return 타입 제네릭 수정

--- src/common/base.service.ts
+++ src/common/base.service.ts
@@ -3,7 +3,7 @@ import { Model } from 'sequelize-typescript';
 
 export default interface BaseService<T extends Model> {
   create(data: Partial<T>): Promise<T>;
-  update?(data: Partial<T>, where: WhereOptions): Promise<T>;
+  update?(data: Partial<T>, where: WhereOptions): Promise<number>;
   delete(id: string, where: WhereOptions): Promise<number>;
   getAll(options: FindOptions): Promise<T[]>;
   getOne(data: Partial<T>): Promise<T>;
