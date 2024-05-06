115fce948a0f75a23a3960b595b77d0328d7fd58
pgkwon1@naver.com <pgkwon1@naver.com>
2024-05-06 20:55:38 +0900

    update: parameter 일관되게 수정

--- src/common/base.service.ts
+++ src/common/base.service.ts
@@ -6,5 +6,5 @@ export default interface BaseService<T extends Model> {
   update?(data: Partial<T>, where: WhereOptions): Promise<number>;
   delete(id: string, where: WhereOptions): Promise<number>;
   getAll(options: FindOptions): Promise<T[]>;
-  getOne(data: Partial<T>): Promise<T>;
+  getOne(options: FindOptions): Promise<T>;
 }
