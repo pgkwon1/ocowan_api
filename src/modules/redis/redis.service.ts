c826243c255a9465360ef5e23941d1ab972418a1
pgkwon1@naver.com <pgkwon1@naver.com>
2024-09-16 22:16:29 +0900

    refactor: redis 기능 개선 (실패시 예외처리)

--- src/modules/redis/redis.service.ts
+++ src/modules/redis/redis.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRedis } from '@songkeys/nestjs-redis';
 import * as dotenv from 'dotenv';
 import Redis, { RedisKey } from 'ioredis';
@@ -10,18 +10,42 @@ export class RedisService {
   constructor(@InjectRedis() private readonly redisClient: Redis) {}
 
   async hashSetValue(key: RedisKey, data: Record<string, string>) {
-    return this.redisClient.hset(key, data);
+    return new Promise((resolve) => {
+      this.redisClient.hset(key, data, (err, result) => {
+        if (err)
+          throw new HttpException('알 수 없는 오류가 발생하였습니다', 500);
+        resolve(result);
+      });
+    });
   }
 
-  async hashGetValue(key: RedisKey, field: string) {
-    return this.redisClient.hget(key, field);
+  async hashGetValue(key: RedisKey, field: string): Promise<string> {
+    return new Promise((resolve) => {
+      this.redisClient.hget(key, field, (err, result) => {
+        if (err)
+          throw new HttpException('알 수 없는 오류가 발생하였습니다', 500);
+        resolve(result);
+      });
+    });
   }
 
-  async setValue(key: RedisKey, value: string) {
-    return this.redisClient.set(key, value);
+  async setValue(key: RedisKey, value: string): Promise<number> {
+    return new Promise((resolve) => {
+      this.redisClient.set(key, value, 'GET', (err, result) => {
+        if (err)
+          throw new HttpException('알 수 없는 오류가 발생하였습니다', 500);
+        resolve(Number(result));
+      });
+    });
   }
 
   async getValue(key: RedisKey) {
-    return this.redisClient.get(key);
+    return new Promise((resolve) => {
+      this.redisClient.get(key, (err, result) => {
+        if (err)
+          throw new HttpException('알 수 없는 오류가 발생하였습니다', 500);
+        resolve(result);
+      });
+    });
   }
 }
