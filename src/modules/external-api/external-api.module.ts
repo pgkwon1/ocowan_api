eb3379381795abe59623411896e207c99f53604f
pgkwon1 <pgkwon1@naver.com>
2025-04-18 22:54:45 +0900

    FIX: TYPO

--- src/modules/external-api/external-api.module.ts
+++ src/modules/external-api/external-api.module.ts
@@ -2,7 +2,6 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ExternalApiService } from './external-api.service';
 
-export const API_GITHUB_SERVICE = 'API_GITHUB_ENDPOINT';
 @Module({
   imports: [HttpModule.register({})],
   providers: [ExternalApiService],
